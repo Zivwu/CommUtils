3ce3c69bc15a58314ad7f43b48ba2ada16220bef
吴明 <>
2018-07-09 23:06:45 +0800

    init

--- app/src/main/java/com/zivwu/commutils_sample/MainActivity.java
+++ app/src/main/java/com/zivwu/commutils_sample/MainActivity.java
@@ -2,6 +2,9 @@ package com.zivwu.commutils_sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.zivwu.commutils.Test;
 
 public class MainActivity extends AppCompatActivity {
 
